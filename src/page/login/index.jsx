c7c8c34ba5b4d0f525dbfbf36821e79f57b96384
Benjamin Huang <bhuang@rms.com>
2018-08-09 11:09:30 -0700

    login style

--- src/page/login/index.jsx
+++ src/page/login/index.jsx
@@ -21,19 +21,14 @@ class Login extends React.Component {
                     <div className="panel-body">
                         <form>
                             <div className="form-group">
-                                <label for="exampleInputEmail1">Email address</label>
-                                <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Email" />>
-                        </div>
-                            <div className="form-group">
-                                <label for="exampleInputPassword1">Password</label>
-                                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />>
-                        </div>
-                            <div className="checkbox">
-                                <label>
-                                    <input type="checkbox" /> Check me out
-                            </label>
+                                <label htmlFor="exampleInputEmail1">Email address</label>
+                                <input type="email" className="form-control" placeholder="Email" />
                             </div>
-                            <button type="submit" className="btn btn-default">Submit</button>
+                            <div className="form-group">
+                                <label htmlFor="exampleInputPassword1">Password</label>
+                                <input type="password" className="form-control" placeholder="Password" />
+                            </div>
+                            <button type="submit" className="btn btn-primary btn-lg btn-block">Submit</button>
                         </form>
                     </div>
                 </div>
